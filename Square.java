e164734e1e6e32d29b3e6071a73a4ada5c727db6
yusuferenkaya <63504037+yusuferenkaya@users.noreply.github.com>
2020-04-28 20:53:05 +0300

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,14 +1,20 @@
-package board;
+
+ package board;
+
+import pieces.Piece;
 
 import javax.swing.*;
 
 public class Square {
 
     int row;
-    int column;
+    private String column;
     boolean isAtSameColumn = false;
     boolean isNeighborColumn = false;
-    public Square (int row,int column){
+    private Piece Piece;
+
+
+    public Square (int row,String column){
         this.row = row;
         this.column = column;
 
@@ -17,7 +23,7 @@ public class Square {
     }
 
     public boolean isAtSameColumn(Square targetLocation) {
-        if(column == targetLocation.column){
+        if(column.equals(targetLocation.column)){
             return true;
         }
        else {
@@ -30,13 +36,226 @@ public class Square {
 
     public double getRowDistance(Square targetLocation){
         return Math.sqrt((targetLocation.row * targetLocation.row - this.row * this.row));
+
+    }
+
+    public int getColumnDistance(Square targetLocation) {
+        targetLocation.column = targetLocation.column.toUpperCase();
+        if (this.column.equals("A") && targetLocation.column.equals("B")) {
+            return 1;
+        } else if (this.column.equals("A") && targetLocation.column.equals("C")){
+            return 2;
+        }
+        else if (this.column.equals("A") && targetLocation.column.equals("D")){
+            return 3;
+        }
+        else if (this.column.equals("A") && targetLocation.column.equals("E")){
+            return 4;
+        }
+        else if (this.column.equals("A") && targetLocation.column.equals("F")){
+            return 5;
+        }
+        else if (this.column.equals("A") && targetLocation.column.equals("G")){
+            return 6;
+        }
+        else if (this.column.equals("A") && targetLocation.column.equals("H")){
+            return 7;
+        }
+        else if (this.column.equals("B") && targetLocation.column.equals("A")){
+            return 1;
+        }
+        else if (this.column.equals("B") && targetLocation.column.equals("C")){
+            return 1;
+        }
+        else if (this.column.equals("B") && targetLocation.column.equals("D")){
+            return 2;
+        }
+        else if (this.column.equals("B") && targetLocation.column.equals("E")) {
+        return 3;
+        }
+        else if (this.column.equals("B") && targetLocation.column.equals("F")){
+            return 4;
+        }
+        else if (this.column.equals("B") && targetLocation.column.equals("G")){
+            return 5;
+        }
+        else if (this.column.equals("B") && targetLocation.column.equals("H")){
+            return 6;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("A")){
+            return 2;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("B")){
+            return 1;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("D")){
+            return 1;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("E")){
+            return 2;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("F")){
+            return 3;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("G")){
+            return 4;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("H")) {
+            return 5;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("A")){
+            return 3;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("B")){
+            return 2;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("C")){
+            return 1;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("E")){
+            return 1;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("F")){
+            return 2;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("G")){
+            return 3;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("H")){
+            return 4;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("A")){
+            return 4;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("B")){
+            return 3;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("C")){
+            return 2;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("D")){
+            return 1;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("F")){
+            return 1;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("G")){
+            return 2;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("H")){
+            return 3;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("A")){
+            return 5;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("B")){
+            return 4;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("C")){
+            return 3;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("D")){
+            return 2;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("E")){
+            return 1;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("G")){
+            return 1;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("H")){
+            return 2;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("A")){
+            return 6;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("B")){
+            return 5;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("C")){
+            return 4;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("D")){
+            return 3;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("E")){
+            return 2;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("F")){
+            return 1;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("H")){
+            return 1;
+        }
+        else if (this.column.equals("H") && targetLocation.column.equals("A")){
+            return 7;
+        }
+        else if (this.column.equals("H") && targetLocation.column.equals("B")){
+            return 6;
+        }
+        else if (this.column.equals("H") && targetLocation.column.equals("C")){
+            return 5;
+        }
+        else if (this.column.equals("H") && targetLocation.column.equals("D")){
+            return 4;
+        }
+        else if (this.column.equals("H") && targetLocation.column.equals("E")){
+            return 3;
+        }
+        else if (this.column.equals("H") && targetLocation.column.equals("F")){
+            return 2;
+        }
+        else if (this.column.equals("H") && targetLocation.column.equals("G")){
+            return 1;
+        }
+        return 0;
+
+
+
     }
 
     public  boolean isNeighborColumn (Square targetLocation) {
-        if (column + 1 == targetLocation.column && column != 8){
+        targetLocation.column = targetLocation.column.toUpperCase();
+        if (this.column.equals("A") && targetLocation.column.equals("B")){
             return true;
         }
-        else if(column - 1 == targetLocation.column && column != 1){
+        else if (this.column.equals("B") && targetLocation.column.equals("C")){
+            return true;
+        }
+        else if (this.column.equals("B") && targetLocation.column.equals("A")){
+            return true;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("D")) {
+            return true;
+        }
+        else if (this.column.equals("C") && targetLocation.column.equals("B")) {
+            return true;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("E")) {
+            return true;
+        }
+        else if (this.column.equals("D") && targetLocation.column.equals("C")) {
+            return true;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("F")) {
+            return true;
+        }
+        else if (this.column.equals("E") && targetLocation.column.equals("D")) {
+            return true;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("G")) {
+            return true;
+        }
+        else if (this.column.equals("F") && targetLocation.column.equals("E")) {
+            return true;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("H")) {
+            return true;
+        }
+        else if (this.column.equals("G") && targetLocation.column.equals("F")) {
+            return true;
+        }
+        else if (this.column.equals("H") && targetLocation.column.equals("G")) {
             return true;
         }
         else{
@@ -45,17 +264,30 @@ public class Square {
     }
 
 
-    public Object getBoard() {
+    public Square getBoard() {
 
+return null;
     }
 
     public Square[] getSquaresBetween(Square location,Square targetLocation){
+
+
 
         return null;
 
 
 
 
+
+    }
+
+
+    public Piece getPiece() {
+        return Piece;
+    }
+
+
+    public boolean isEmpty() {
 
     }
 }
